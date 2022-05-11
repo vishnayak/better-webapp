569a32afe4afac621602bd3b260091657b5145bb
Vishwesh Nayak <vkrishnanand@umass.edu>
2022-05-11 14:56:13 -0400

    add translate button and translate all button to search hits

--- src/components/search-hit-card/SearchHitCard.tsx
+++ src/components/search-hit-card/SearchHitCard.tsx
@@ -4,52 +4,51 @@ import React from 'react';
 import './SearchHitCard.css';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
-import { translateText } from '@services/translation-service';
-
 export interface SearchHitCardProps {
     searchHit: SearchHit;
+    showTranslated: boolean;
 }
 
-export const SearchHitCard: React.FC<SearchHitCardProps> = ({ searchHit }) => {
-    const [text, setText] = React.useState(searchHit.docText);
+export const SearchHitCard: React.FC<SearchHitCardProps> = ({ searchHit, showTranslated }) => {
+    const [text, setText] = React.useState(showTranslated ? searchHit.translatedDocText : searchHit.docText);
     const [expanded, setExpanded] = React.useState(false);
     const [translateButtonLabel, setTranslateButtonLabel] = React.useState('Translate');
-    const [translatedSentences, setTranslatedSentences] = React.useState<Record<number, string>>({});
-    const [currTranslatedIndex, setCurrTranslatedIndex] = React.useState(-1);
     const TRANSLATE = 'Translate';
     const SHOW_ORIGINAL = 'Show Original';
-    const TRANSLATING = 'Translating...';
 
     const translate = () => {
         if(translateButtonLabel === TRANSLATE) {
-            searchHit.sentenceRanges.forEach((sentence, i) => {
-                translateText(sentence.text).then(res => {
-                    setTranslatedSentences(prev => ({ ...prev, [i]: res }));
-                });
-            });
-            setTranslateButtonLabel(TRANSLATING);
+            setText(searchHit.translatedDocText);
+            setTranslateButtonLabel(SHOW_ORIGINAL);
         } else {
             setText(searchHit.docText);
             setTranslateButtonLabel(TRANSLATE);
-            setCurrTranslatedIndex(-1);
         }
     }
 
+    // React.useEffect(() => {
+    //     if(translatedSentences[currTranslatedIndex + 1]) {
+    //         let tempText = currTranslatedIndex === -1 ? '' : text;
+    //         let tempIndex = currTranslatedIndex + 1;
+    //         while(translatedSentences[tempIndex]) {
+    //             tempText += '\n' + translatedSentences[tempIndex++];
+    //         }
+    //         setText(tempText);
+    //         setCurrTranslatedIndex(tempIndex - 1);
+    //         if(tempIndex === searchHit.sentenceRanges.length) {
+    //             setCurrTranslatedIndex(-1);
+    //             setTranslateButtonLabel(SHOW_ORIGINAL);
+    //         }
+    //     }
+    // }, [translatedSentences]);
+
     React.useEffect(() => {
-        if(translatedSentences[currTranslatedIndex + 1]) {
-            let tempText = currTranslatedIndex === -1 ? '' : text;
-            let tempIndex = currTranslatedIndex + 1;
-            while(translatedSentences[tempIndex]) {
-                tempText += '\n' + translatedSentences[tempIndex++];
-            }
-            setText(tempText);
-            setCurrTranslatedIndex(tempIndex - 1);
-            if(tempIndex === searchHit.sentenceRanges.length) {
-                setCurrTranslatedIndex(-1);
-                setTranslateButtonLabel(SHOW_ORIGINAL);
-            }
+        if(showTranslated) setText(searchHit.translatedDocText);
+        else { 
+            setTranslateButtonLabel(TRANSLATE);
+            setText(searchHit.docText);
         }
-    }, [translatedSentences]);
+    }, [showTranslated, searchHit.translatedDocText, searchHit.docText]);
 
     const insertNewLines = (text: string) => {
         return <>{text.split('\n').map((t, index) => {
@@ -70,10 +69,7 @@ export const SearchHitCard: React.FC<SearchHitCardProps> = ({ searchHit }) => {
                 <KeyboardArrowDownIcon fontSize={'large'} classes={{root: 'up-arrow'}} /> : 
                 <KeyboardArrowDownIcon fontSize={'large'} />}
             </IconButton>
-            {translateButtonLabel === TRANSLATING && <Box sx={{ width: '50%' }}>
-                <LinearProgress />
-            </Box>}
-            {/* <Button size="small" disabled={translateButtonLabel === TRANSLATING} onClick={translate}>{translateButtonLabel}</Button> */}
+            {!showTranslated && <Button size="small" onClick={translate}>{translateButtonLabel}</Button>}
         </CardActions>
     </Card>;
 }
