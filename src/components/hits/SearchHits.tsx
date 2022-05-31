54e96c3867811193189276ce8ec4ee5873f0cec2
Vishwesh Nayak <vishu.nayak1@gmail.com>
2022-05-31 18:47:51 -0400

    add event filter to submission hits page

--- src/components/hits/SearchHits.tsx
+++ src/components/hits/SearchHits.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Pagination from '@mui/material/Pagination';
 import './SearchHits.css';
 import { SearchHitCard } from '@components/search-hit-card/SearchHitCard';
-import { Button, FormControl, MenuItem, Select, SelectChangeEvent } from '@mui/material';
+import { Button, Chip, FormControl, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent } from '@mui/material';
+import { Box } from '@mui/system';
 
 interface SearchHitsProps {
     hits: SearchHit[];
@@ -38,6 +39,22 @@ export interface Sentence {
 const TRANSLATE_ALL = 'Translate All';
 const SHOW_ORIGINAL = 'Show Original';
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+            maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+            width: 250,
+        },
+    },
+};
+
+interface EventFilterOption {
+    name: string;
+    count: number;
+};
+
 export const SearchHits: React.FC<SearchHitsProps> = ({ hits }) => {
     const allIndices: number[] = [];
     const relevantIndices: number[] = [];
@@ -50,6 +67,7 @@ export const SearchHits: React.FC<SearchHitsProps> = ({ hits }) => {
     const [page, setPage] = React.useState(1);
     const [translateAll, setTranslateAll] = React.useState<boolean>(false);
     const [showRelevant, setShowRelevant] = React.useState<boolean>(false);
+    const [filterEvents, setFilterEvents] = React.useState<string[]>([]);
     const pageSize = 20;
     const handlePageChange = (e: any, pageNum: number) => { setPage(pageNum); };
     const handleTranslateAllClick = () => {
@@ -61,23 +79,68 @@ export const SearchHits: React.FC<SearchHitsProps> = ({ hits }) => {
         setPage(1);
     };
 
+    const handleEventFilterChange = (e: SelectChangeEvent) => {
+        setFilterEvents(e.target.value as unknown as string[]);
+    };
 
-    const indicesToShow = showRelevant ? relevantIndices : allIndices;
+    const allEvents: EventFilterOption[] = Object.entries(hits.reduce((res: Record<string, number>, h) => {
+        Object.entries(h.eventCounts).forEach(entry => {
+            res[entry[0]] = (entry[1] || 0) + 1;
+        });
+        return res;
+    }, {})).map(entry => ({name: entry[0], count: entry[1]}));
+
+    let indicesToShow = showRelevant ? relevantIndices : allIndices;
+    if(filterEvents.length > 0) {
+        indicesToShow = indicesToShow.filter(i => Object.keys(hits[i].eventCounts).length > 0 && filterEvents.some(ev => hits[i].eventCounts[ev] !== undefined));
+    }
+    
     const pgNo = Math.ceil(indicesToShow.length/pageSize);
 
     return hits.length > 0 ? <>
         <Pagination classes={{ root: 'search-hits-pagination' }} count = {pgNo} page={page} onChange={handlePageChange}/>
         <div className={'pagination-text-section'}>
             <div className={'pagination-text'}>{`Showing ${Math.min((page-1)*pageSize + 1, indicesToShow.length)} - ${Math.min(page*pageSize, indicesToShow.length)} of ${indicesToShow.length} hits`}</div>
-            <FormControl variant='standard' sx={{ m: 1, minWidth: 120 }}>
+            <FormControl sx={{ m: 1, width: 180 }}>
+                <InputLabel id="relevance-select-label">Relevance</InputLabel>
                 <Select
-                    labelId={'filter-label'}
+                    labelId={'relevance-select-label'}
                     value={showRelevant ? 1 : 0}
+                    input={<OutlinedInput label="Relevance" />}
                     onChange={handleFilterChange}
-                    label='Filter Hits'
+                    label='Relevance'
                 >
                     <MenuItem value={0}>All hits</MenuItem>
                     <MenuItem value={1}>Relevant Hits</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl sx={{ m: 1, width: 350 }}>
+                <InputLabel id="event-select-label">Events</InputLabel>
+                <Select
+                    labelId="event-select-label"
+                    multiple
+                    value={filterEvents as unknown as string}
+                    onChange={handleEventFilterChange}
+                    input={<OutlinedInput label="Events" />}
+                    renderValue={(selected) => (
+                        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+                            {(selected as unknown as string[]).map((value) => (
+                                <Chip size='small' key={value} label={value} />
+                            ))}
+                        </Box>
+                    )}
+                    MenuProps={MenuProps}
+                >
+                    {
+                        allEvents.map((event) => (
+                            <MenuItem
+                                key={event.name}
+                                value={event.name}
+                            >
+                                {event.name}
+                            </MenuItem>
+                        ))
+                    }
                 </Select>
             </FormControl>
             {/* <Button variant={'outlined'} onClick={handleFilterToggle}>{showRelevant ? SHOW_ALL : SHOW_RELEVANT}</Button> */}
